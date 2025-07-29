2c99e27e91b921a336f3fe18fcd23e545ce07606
hukum basnet <basnethukum789@gmail.com>
2025-07-29 11:26:42 +0545

    Reverse the string

---
+++ reverseString.ts
@@ -0,0 +1,9 @@
+function reverseString(str: string): string {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString("hukum")); // Output: "mukuh"
