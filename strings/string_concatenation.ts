a38d63ea1293cc378c472726243da5a73e89dbde
hukum basnet <basnethukum789@gmail.com>
2025-08-10 17:58:22 +0000

    String concatenation

---
+++ strings/string_concatenation.ts
@@ -0,0 +1,9 @@
+function concatenateStrings(str1: string, str2: string): string {
+    const result = str1 + str2;
+    return result;
+}
+
+// Usage
+const a = "Hello, ";
+const b = "World!";
+console.log(concatenateStrings(a, b));
